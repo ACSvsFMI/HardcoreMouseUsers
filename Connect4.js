66370e8b2eb99db32fdb8c3feb5fdd334970c719
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2012-12-08 20:02:29 +0200

    fix arr not defined

--- Connect4.js
+++ Connect4.js
@@ -227,7 +227,7 @@ function resetGrid() {
   console.log(keys);
   for(var i = 0; i < keys.length; i++) {
     if(keys[i] == "lastPlayer") {
-      arr.splice(i, 1);
+      keys.splice(i, 1);
       break;
     }
   }
